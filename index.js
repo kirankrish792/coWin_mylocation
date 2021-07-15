bd65fd456bacfd8b2a979d0553808ebb85a22eb3
Kiran KS <kirankrish792@gmail.com>
2021-07-15 13:59:07 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,60 @@
+
+let today = new Date();
+let dd = today.getDate();
+let mm = today.getMonth() + 1;
+let yyyy = today.getFullYear();
+const data = document.querySelector('.data');
+
+
+const buildList = (centers, i) => {
+    const div = document.createElement('div');
+    const h3 = document.createElement('h3');
+    const h4 = document.createElement('h4');
+    const p = document.createElement('p');
+    const h5 = document.createElement('h5');
+
+    div.className = 'box';
+    h3.append(centers[i].name);
+    h4.append("Availability: ", centers[i].sessions[0].available_capacity_dose1);
+    p.append('Date: ', centers[i].sessions[0].date);
+    h5.append(centers[i].sessions[0].vaccine)
+    if (centers[i].sessions[0].available_capacity_dose1 !== 0) {
+        const button = document.createElement('button');
+        button.innerHTML = "<a href=https://selfregistration.cowin.gov.in/>Book Your Slot !!</a>"
+        div.append(h3, h4, h5, p, button);
+    }
+    else {
+        div.append(h3, h4, h5, p);
+    }
+
+    data.append(div);
+
+}
+
+const list = async () => {
+    var box = document.querySelectorAll('.box');
+    if (typeof (box[0]) !== 'undefined' && box !== null) {
+        for (let j = 0; j < box.length; j++) {
+            box[j].remove();
+        }
+    }
+    const cowinListToday = await axios.get(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=305&date=${dd}-${mm}-${yyyy}`);
+    let centers = cowinListToday.data.centers;
+    for (let i = 0; i < centers.length; i++) {
+        if (centers[i].sessions[0].available_capacity_dose1 !== 0) {
+            buildList(centers, i)
+        }
+        else {
+            const div = document.createElement('div');
+            div.className = 'box';
+            const h2 = document.createElement('h2');
+            h2.append('No slots available');
+            div.append(h2);
+            data.append(div);
+            break;
+        }
+    }
+}
+
+list();
+setInterval(list, 5000);
